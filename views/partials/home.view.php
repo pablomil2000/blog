a3fc0e645ddea3b3cc4d4091902fa54c780150d4
pablomil2000 <martinlopezpablo@gmail.com>
2025-04-22 14:50:20 +0200

    Vista home actializada

--- views/partials/home.view.php
+++ views/partials/home.view.php
@@ -19,7 +19,7 @@
         </p>
         <p><span class="glyphicon glyphicon-time"></span> Posted on <?= $post['Creation'] ?></p>
         <p><?= $post['Content'] ?></p>
-        <a class="btn btn-default" href="<?= $template->baseUrl ?>post/<?= $post['Slug'] ?>">Read More</a>
+        <a class="btn btn-primary" href="<?= $template->baseUrl ?>post/<?= $post['Slug'] ?>">Read More</a>
       </div>
     <?php
     }
--- views/partials/search.view.php
+++ views/partials/search.view.php
@@ -1,5 +1,5 @@
-<form action="" method="post">
+<form action="" method="post" id="searchForm">
     <h1>Buscar post</h1>
-    <input type="search" name="search" value="<?= $_SERVER['REQUEST_METHOD'] == 'POST' ? $_POST['search'] : '' ?>">
-    <button type="submit">Buscar</button>
+    <input id="search" type="search" name="search" value="<?= $_SERVER['REQUEST_METHOD'] == 'POST' ? $_POST['search'] : '' ?>">
+    <button type="submit" class="btn btn-primary">Buscar</button>
 </form>
